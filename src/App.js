6d62230fefb8953249b3848af4e36476e315d536
Amey <ameybirute3@gmail.com>
2024-03-18 23:37:15 +0530

    dark name changed

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ const App = () => {
     };
 
     return (
-        <div className={`app ${isLightMode ? 'light-mode' : 'dark-mode'}`}>
+        <div className={`app ${isLightMode ? 'light-mode' : 'kala-mode'}`}>
             <button className="light-mode-button" onClick={toggleLightMode}>
                 {isLightMode ? 'Switch to Dark Mode' : 'Switch to Light Mode'}
             </button>
